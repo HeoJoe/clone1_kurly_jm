bac77ccc4006143b48b1a35d7b02914e126cf552
jung0115 <jungim7490@gmail.com>
2024-01-18 00:41:46 +0900

    [ADD] start project

---
+++ src/page/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import palette from "../styles/colorPalette";
+
+const Home = () => {
+  return (
+    <Container>
+      dd
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: block;
+  position: relative;
+  height: max-content;
+  min-height: 100vh;
+`;
+
+export default Home;
