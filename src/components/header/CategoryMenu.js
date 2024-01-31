a36691eed3a2a3cb246c885c27b456732d4aa947
jung0115 <jungim7490@gmail.com>
2024-02-01 01:31:32 +0900

    [FEAT] #2 - 카테고리 메뉴 > 하위 카테고리 진행 중

--- src/components/header/CategoryMenu.js
+++ src/components/header/CategoryMenu.js
@@ -10,8 +10,10 @@ import RecommendIconUnselect from "../../contents/ic_category_recommend_unselect
 import RecommendIconSelect from "../../contents/ic_category_recommend_select.webp";
 
 const CategoryMenu = () => {
-  const categories = [{"unselectIcon": BakeryIconUnselect, "selectIcon": BakeryIconSelect, "title": "베이커리"},
-                      {"unselectIcon": RecommendIconUnselect, "selectIcon": RecommendIconSelect, "title": "컬리의 추천"}];
+  const categories = [{"unselectIcon": BakeryIconUnselect, "selectIcon": BakeryIconSelect, "title": "베이커리",
+                        "subCategories": ["잼·스프레드", "식빵·모닝빵·베이글", "간식빵", "타르트·파이", "디저트", "케이크"]},
+                      {"unselectIcon": RecommendIconUnselect, "selectIcon": RecommendIconSelect, "title": "컬리의 추천",
+                        "subCategories": ["홈캉스", "식단관리", "간편한 아침식사", "재구매 BEST", "3천원의 행복", "컬리마트", "대용량 상품", "캠핑", "1인 가구", "비건", "베이커리 맛집", "오프라인 맛집", "컬리가 만든 상품", "Kurly Only", "KF365 / KS365", "희소가치 프로젝트"]}];                        
   const [selectCategory, setSelectCategory] = useState(-1);
   
   return(
@@ -52,7 +54,7 @@ const Container = styled.div`
   border: 1px solid ${palette.grayDD};
   flex-direction: column;
   z-index: 1000;
-  
+
   :hover {
     background: ${palette.grayEE};
   }
