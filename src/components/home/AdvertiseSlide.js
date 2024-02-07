2166c86512ae8131859d76cb44c55031389c7c12
jung0115 <jungim7490@gmail.com>
2024-02-07 22:44:19 +0900

    [UPDATE] #2 - 광고 캐러셀 라이브러리로 변경 중(에러 있음)

--- src/components/home/AdvertiseSlide.js
+++ src/components/home/AdvertiseSlide.js
@@ -1,6 +1,9 @@
 // 광고 슬라이드
 import React from "react";
 import { useEffect, useState, useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import styled from "styled-components";
 import palette from "../../styles/colorPalette";
 
@@ -12,49 +15,40 @@ import Advertise2 from "../../contents/img_advertise_2.png";
 import Advertise3 from "../../contents/img_advertise_3.jpg";
 
 const AdvertiseSlide = () => {
-  const images = useRef([{ src: "https://product-image.kurly.com/cdn-cgi/image/width=1900,height=370,quality=85/banner/main/pc/img/90d88462-814b-4b58-ab7b-5af55b93a816.jpg"},
-  {src: "https://product-image.kurly.com/cdn-cgi/image/width=1900,height=370,quality=85/banner/main/pc/img/20c56ca7-d335-4198-ad3a-fe717acceaa5.png"},
-  {src: "https://product-image.kurly.com/cdn-cgi/image/width=1900,height=370,quality=85/banner/main/pc/img/be20a6f6-0092-4e4a-9a03-fdcde23163ab.jpg"}]);
 
-  const imgSize = useRef(images.current.length);
-  const [current, setCurrent] = useState(0);
-  const [style, setStyle] = useState({
-    transform: `translateX(-${current}00vw)`
-  });
-
-  const moveSlide = (i) => {
-    let nextIndex = current + i;
-
-    if (nextIndex < 0) nextIndex = imgSize.current - 1;
-    else if (nextIndex >= imgSize.current) nextIndex = 0;
-
-    setCurrent(nextIndex);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 10000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
-  useEffect(() => {
-    setStyle({ transform: `translateX(-${current}00vw)` });
-    console.log(current);
-  }, [current]);
+  const images = [{ src: "https://product-image.kurly.com/cdn-cgi/image/width=1900,height=370,quality=85/banner/main/pc/img/90d88462-814b-4b58-ab7b-5af55b93a816.jpg"},
+  {src: "https://product-image.kurly.com/cdn-cgi/image/width=1900,height=370,quality=85/banner/main/pc/img/20c56ca7-d335-4198-ad3a-fe717acceaa5.png"},
+  {src: "https://product-image.kurly.com/cdn-cgi/image/width=1900,height=370,quality=85/banner/main/pc/img/be20a6f6-0092-4e4a-9a03-fdcde23163ab.jpg"}];
+
 
   return (
     <Container>
-      <AdvertiseContainter style={style}>
-        {images.current.map((img, idx) => (
-          <AdvertiseImg
+      <AdvertiseContainter>
+        <Slider {...settings}>
+          {images.map((img, idx) => 
+            <AdvertiseImg
             key={idx}
-            src={img.src}
-          />
-        ))}
+            src={img.src} />
+          )}
+        </Slider>
       </AdvertiseContainter>
 
-      <SlideMoveBtns>
-        <PreviousBtn onClick={() => { moveSlide(-1); }}>
+      {/*<SlideMoveBtns>
+        <PreviousBtn>
           <IoIosArrowDropleftCircle size="60" />
         </PreviousBtn>
-        <NextBtn onClick={() => { moveSlide(1); }}>
+        <NextBtn>
           <IoIosArrowDroprightCircle size="60" />
         </NextBtn>
-      </SlideMoveBtns>
+        </SlideMoveBtns>*/}
     </Container>
   );
 }
@@ -66,15 +60,14 @@ const Container = styled.div`
   height: max-content;
   top: 190px;
   z-index: 10;
-  overflow: hidden; 
-  justify-content: center;
 `;
 
 const AdvertiseContainter = styled.div`
-  display: flex;
+  height: 360px;
 `;
 const AdvertiseImg = styled.img`
-  height: 360px;
+  max-width: 1900px;
+  height: 370px;
   cursor: pointer;
 `;
 
