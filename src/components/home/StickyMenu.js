8b048f158dc0d282111e36dd7cbbcd4ca5752a24
jung0115 <jungim7490@gmail.com>
2024-03-07 17:48:17 +0900

    [FEAT] #11 #18 - Sticky Menu - 최근 본 상품

--- src/components/home/StickyMenu.js
+++ src/components/home/StickyMenu.js
@@ -6,7 +6,13 @@ import palette from "../../styles/colorPalette";
 
 import ImgDelivery from "../../contents/img_sticky_delivery.jpg";
 
+import { MdKeyboardArrowUp } from "react-icons/md";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
 const StickyMenu = () => {
+  const productImgs = ["https://product-image.kurly.com/cdn-cgi/image/fit=crop,width=120,height=156,quality=85/product/image/c81065a4-6736-4a9e-b260-d32d789c1d97.jpg",
+                      "https://3p-image.kurly.com/cdn-cgi/image/fit=crop,width=120,height=156,quality=85/files/20240109/6018f781-c173-4d8d-81f6-5519d1577851.jpg",
+                      "https://product-image.kurly.com/cdn-cgi/image/fit=crop,width=120,height=156,quality=85/product/image/47c2c4cf-d25b-4188-bd92-1a006e37413e.jpg"];
 
   return(
     <Container>
@@ -18,6 +24,19 @@ const StickyMenu = () => {
           <SubBtn>등급별 혜택</SubBtn>
           <SubBtn>레시피</SubBtn>
         </SubMenu>
+
+        <ViewProductContainer>
+          <MdKeyboardArrowUp size="18" color={palette.grayDD} />
+          최근 본 상품
+          <ProductList>
+            {productImgs.map((image, idx) => 
+              <ProductImg
+                key={idx}
+                src={image}/>
+            )}
+          </ProductList>
+          <MdKeyboardArrowDown size="18" color={palette.black33} />
+        </ViewProductContainer>
       </MenuContainer>
     </Container>
   );
@@ -71,6 +90,32 @@ const SubBtn = styled.div`
   text-align: center;
   border-bottom: 1px solid ${palette.grayDD};
   cursor: pointer;
+`;
+
+const ViewProductContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 8px;
+  border: 1px solid ${palette.grayDD};
+  background-color: ${palette.white};
+  text-align: center;
+  font-weight: 600;
+`;
+const ProductList = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: 209px;
+  overflow: hidden;
+  margin-top: 6px;
+`;
+const ProductImg = styled.img`
+  box-sizing: border-box;
+  display: block;
+  width: initial;
+  height: 80px;
+  margin: 2px;
+  max-width: 100%;
 `;
 
 export default StickyMenu;
