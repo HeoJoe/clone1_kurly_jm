e6fa520b36c0127026f78c149cae8e27874d01db
jung0115 <jungim7490@gmail.com>
2024-03-07 13:51:40 +0900

    [UPDATE] #11 - MD의 추천 > JSON 파일에서 카테고리 정보 가져오기

--- src/components/home/MDRecommend.js
+++ src/components/home/MDRecommend.js
@@ -4,17 +4,15 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import palette from "../../styles/colorPalette";
 
+import MdRecommend from "../../contents/mdRecommendJson.json";
+
 
 const MDRecommend = () => {
-  const categories = ["간편식·밀키트·샐러드", "과일·견과·쌀", "국·반찬·메인요리", "헤어·바디·구강", "정육·가공육·계란", "생수·음료", "커피·차", "베이커리", "스킨케어·메이크업",
-                      "채소", "수산·해산·건어물", "생활용품·리빙", "면·양념·오일", "주방용품", "간식·과자·떡", "전통주", "가전제품", "가구·인테리어", "건강식품", "유아동",
-                      "와인·위스키", "반려동물", "패션잡화"];
-  
   const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
                       
-  const [selectCategory, setSelectCategory] = useState(categories[getRandom(0, 22)]);
+  const [selectCategory, setSelectCategory] = useState(MdRecommend[getRandom(0, 22)].category);
 
   useEffect(() => {
   }, [selectCategory]);
@@ -24,19 +22,19 @@ const MDRecommend = () => {
       <Title>MD의 추천</Title>
 
       <CategoryList>
-        {categories.map((category, idx) =>
+        {MdRecommend.map((recommend, idx) =>
           <>
-            {category == selectCategory ?
+            {recommend.category == selectCategory ?
               <CategorySelectBtn
                 key={idx}
-                onClick={() => setSelectCategory(category)}>
-                {category}
+                onClick={() => setSelectCategory(recommend.category)}>
+                {recommend.category}
               </CategorySelectBtn>
             :
               <CategoryBtn
                 key={idx}
-                onClick={() => setSelectCategory(category)}>
-                {category}
+                onClick={() => setSelectCategory(recommend.category)}>
+                {recommend.category}
               </CategoryBtn>
             }
           </>
